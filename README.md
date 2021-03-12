59d708fe52283fd8a4bd2b7be8a767af1b493a2f
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2021-03-12 09:32:56 -0500

    rebuild repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# rollup quantum boilerplate
+
+[![Discord](https://img.shields.io/discord/714863701802352671.svg?label=&logo=discord&logoColor=ffffff&color=7389D8&labelColor=6A7EC2)](https://discord.gg/enBATx9)
---
+++ test/index.js
@@ -0,0 +1,6 @@
+import '/node_modules/@javascribble/quantum/source/main.js';
+import '/node_modules/@javascribble/quantum-button/source/main.js';
+import '/node_modules/@javascribble/quantum-icon/source/main.js';
+import '/source/main.js';
+
+document.body.style.visibility = 'visible';
